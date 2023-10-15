62314f4205464b2752a966df79f3f580772a39e5
Parsa Hajiha <hajihaparsa2004@gmail.com>
2023-10-15 00:09:35 -0500

    first commit

---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+    // Temp fonts
+    fonts: {
+      title: "Space Grotesk, sans-serif",
+      main: "Space Grotesk, sans-serif"
+    },
+    // Colors for layout
+    colors: {
+      primary1: "#0c4a28",
+      background1: "#0c4a28",
+      button: "#854CE6",
+      background2: "#0c4a28",
+      text: "#F2F3F4",
+      text1: "#F2F3F4",
+      text2: "#F2F3F4",
+      text3: "#F2F3F4",
+      footerBackground: "#00012B"
+    },
+    // Breakpoints for responsive design
+    breakpoints: {
+      sm: 'screen and (max-width: 640px)',
+      md: 'screen and (max-width: 768px)',
+      lg: 'screen and (max-width: 1024px)',
+      xl: 'screen and (max-width: 1280px)'
+    },
+  }
