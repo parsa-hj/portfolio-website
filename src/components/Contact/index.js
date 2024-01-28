1ec416189f86efb6c0672f2cabc0328d524298f0
Parsa Hajiha <hajihaparsa2004@gmail.com>
2024-01-27 21:46:33 -0600

    contact form updated

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -153,7 +153,6 @@ const Contact = () => {
           <ContactTitle>Email Me 🚀</ContactTitle>
           <ContactInput placeholder="Your Email" name="from_email" />
           <ContactInput placeholder="Your Name" name="from_name" />
-          <ContactInput placeholder="Subject" name="subject" />
           <ContactInputMessage placeholder="Message" rows="4" name="message" />
           <ContactButton type="submit" value="Send" />
         </ContactForm>
